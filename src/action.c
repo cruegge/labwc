1226e46c93756e13c10379f8a4f8953b7cbfc519
Christoph Ruegge <mail@cxcs.de>
2025-06-18 00:24:38 +0200

    stretch

--- src/action.c
+++ src/action.c
@@ -139,6 +139,7 @@ struct action_arg_list {
 	X(TOGGLE_SHOW_DESKTOP, "ToggleShowDesktop") \
 	X(WARP_CURSOR, "WarpCursor") \
 	X(HIDE_CURSOR, "HideCursor") \
+	X(RESIZE_STRETCH, "ResizeStretch") \
 	X(DEBUG_TOGGLE_KEY_STATE_INDICATOR, "DebugToggleKeyStateIndicator")
 
 /*
@@ -443,6 +444,16 @@ action_arg_from_xml_node(struct action *action, const char *nodename, const char
 		if (!strcmp(argument, "left") || !strcmp(argument, "right") ||
 				!strcmp(argument, "top") || !strcmp(argument, "bottom")) {
 			action_arg_add_int(action, argument, atoi(content));
+			goto cleanup;
+		}
+		break;
+	case ACTION_TYPE_RESIZE_STRETCH:
+		if (!strcmp(argument, "horizontal") || !strcmp(argument, "vertical") ||
+				!strcmp(argument, "hdivs") || !strcmp(argument, "vdivs")) {
+			action_arg_add_int(action, argument, atoi(content));
+			goto cleanup;
+		} else if (!strcasecmp(argument, "keepAspect")) {
+			action_arg_add_bool(action, argument, parse_bool(content, false));
 			goto cleanup;
 		}
 		break;
@@ -1295,6 +1306,16 @@ run_action(struct view *view, struct action *action,
 			int top = action_get_int(action, "top", 0);
 			int bottom = action_get_int(action, "bottom", 0);
 			view_resize_relative(view, left, right, top, bottom);
+		}
+		break;
+	case ACTION_TYPE_RESIZE_STRETCH:
+		if (view) {
+			int horizontal = action_get_int(action, "horizontal", 0);
+			int vertical = action_get_int(action, "vertical", 0);
+			int hdivs = action_get_int(action, "hdivs", 0);
+			int vdivs = action_get_int(action, "vdivs", 0);
+			bool keep_aspect= action_get_bool(action, "keepAspect", false);
+			view_resize_stretch(view, horizontal, vertical, hdivs, vdivs, keep_aspect);
 		}
 		break;
 	case ACTION_TYPE_MOVETO:
